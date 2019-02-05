579642f64dfc7f7e15acf250fa228bb3c8b93b6b
eduardolc2 <edu_10agro@hotmail.com>
2019-02-05 14:21:48 -0300

    Introdução 1/2

---
+++ Introdução/primeiro-projeto/src/app/meu-primeiro-component/meu-primeiro-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+    <p> Meu Primeiro componente</p>
+    `
+})
+export class MeuPrimeiroComponent {
+
+}
