3569415989bc7062eccb2373e031ddabb6f8bcd6
eduardolc2 <edu_10agro@hotmail.com>
2019-02-05 17:30:14 -0300

    Introdução Aula 2 completada

---
+++ Introdução/hello-typescript/main.js
@@ -0,0 +1,19 @@
+var minhaVariavel = "Empresa Procenge";
+function somaNumeros(a, b) {
+    return a + b;
+}
+var num = 0;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; });
+var math = /** @class */ (function () {
+    function math() {
+    }
+    math.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return math;
+}());
---
+++ Introdução/hello-typescript/main.ts
@@ -0,0 +1,23 @@
+var minhaVariavel = "Empresa Procenge";
+
+function somaNumeros1( a, b) {
+    return a + b;
+}
+
+let num1 = 0;
+
+const pi1 = 3.14;
+
+var numeros = [1, 2, 3];
+
+numeros.map(function(valor) {
+    return valor * 2;
+})
+
+numeros.map(valor => valor * 2);
+
+class math1 {
+    soma(x, y) {
+        return x + y;
+    }
+}
